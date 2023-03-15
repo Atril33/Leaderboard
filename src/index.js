9311da0ae929a54cf523a25f00eb235e6f85a1b7
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-03-15 21:55:51 +0500

    Add Codes and Linters Files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import myScoreItems from './modules/function.js';
+import './modules/DisplayData.js';
+import './modules/AddData.js';
 import './style.css';
-
-for (let i = 1; i < myScoreItems.length; i += 2) {
-  myScoreItems[i].style.backgroundColor = '#dddddd';
-}
